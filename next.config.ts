4d0b519952a6dd006c6630a3fd4102a8fb1afb60
peth02 <oopeth139oo@gmail.com>
2025-12-22 15:53:31 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
+};
 export default nextConfig;
