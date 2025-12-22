19279254296214a9b33d7a9777e2f69f91ccd17d
peth02 <oopeth139oo@gmail.com>
2025-12-22 23:44:13 +0800

    add navbar dynamic style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,25 @@ import Link from "next/link";
 import { loginAction, logoutAction } from "@/lib/action";
 import { getUser } from "@/lib/session";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, redirect } from "next/navigation";
 
 export function HomeNavbar() {
+
   const [user, setUser] = useState<any>(null);
-  const router = useRouter();
+  const path = usePathname();
+
+  let pages;
+  if (user) {
+    pages = new Map([
+      ["/home", "Explore"],
+      ["/my_learning", "My Learning"],
+      ["/my_created_courses", "My Created Courses"]
+    ]);
+  } else {
+    pages = new Map([
+      ["/home", "Explore"],
+    ]);
+  }
 
   useEffect(() => {
     async function fetchUser() {
@@ -17,6 +31,7 @@ export function HomeNavbar() {
     }
     fetchUser();
   }, []);
+
   function handleLogin() {
     loginAction();
     location.reload();
@@ -24,8 +39,9 @@ export function HomeNavbar() {
   function handleLogout() {
     logoutAction();
     setUser(null);
-    location.reload();
+    redirect('/home');
   }
+
   return (
     <nav className="min-w-[250px] w-auto h-screen bg-gray-200 px-4 py-10 sticky top-0">
       <div className="flex flex-col h-full">
@@ -33,11 +49,20 @@ export function HomeNavbar() {
           <p>Myapp Learning Platform</p>
           <hr className="my-6" />
         </div>
-        <ul>
-            <li><Link href={"/home"} className="">Explore</Link></li>
-            <li><Link href={"/home"} className="">My Learning</Link></li>
-            <li><Link href={"/home"} className="">My Created Courses</Link></li>
-        </ul>
+        <div className="flex flex-col gap-3">
+            {
+              [...pages.entries()].map(([key, value])=>(
+                <Link 
+                  key={key} 
+                  href={key} 
+                  className={path.includes(key) 
+                    ? "flex items-center gap-3 px-4 py-3 rounded-lg text-blue-500 bg-blue-100 font-bold" 
+                    : "flex items-center gap-3 px-4 py-3 rounded-lg hover:text-blue-500 hover:bg-blue-100 hover:font-bold"}>
+                      {value}
+                </Link>
+              ))
+            }
+        </div>
         <div className="flex flex-col mt-auto">
             <hr className="my-4" />
             { user ? (
