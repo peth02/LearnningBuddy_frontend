d3f3c39e457007a1bb9b4f4473ddd1d9013523ed
peth02 <oopeth139oo@gmail.com>
2025-12-23 00:49:47 +0800

    add search bar

--- app/(Home)/home/page.tsx
+++ app/(Home)/home/page.tsx
@@ -1,42 +1,34 @@
+"use client"
+import { useState } from "react";
+
 export default function Home() {
+  const [search, setSearch] = useState<string>("");
+
+  const handleSearch = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(search)
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+    <div className="flex min-h-screen flex-col gap-5 p-24">
+      <h1>Explore Courses</h1>
+      <div className="flex border border-gray-300 rounded-lg px-5 w-full gap-5">
+        <div className="py-2">icon</div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="outline-none py-2 w-full"
+          value={search}
+          onChange={handleSearch}
+        />
+        {
+          search ? (
+            <div className="py-2" onClick={()=>{setSearch("")}}>delete</div>
+          ) : (
+            <></>
+          )
+        }
+      </div>
+      <div>items</div>
     </div>
   );
 }
