1e5b15cef790fd0affcd66dd808fb29cbdf8eb14
peth02 <oopeth139oo@gmail.com>
2025-12-25 17:10:47 +0800

    create grid to show 12 data / page

--- app/(Home)/home/page.tsx
+++ app/(Home)/home/page.tsx
@@ -1,36 +1,46 @@
 "use client";
 import CoursesCard from "@/components/CoursesCard";
 import { SearchBar } from "@/components/Searchbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getPokemon } from "@/service/pokemon";
 
 export default function Home() {
   const [search, setSearch] = useState<string>("");
+  const [data, setData] = useState([]);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(search);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getPokemon();
+        console.log("Data loaded:", response.results);
+        setData(response.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen gap-5 py-10 px-20 bg-gray-100">
+    <div className="flex flex-col min-h-screen gap-7 py-10 px-20 bg-gray-100">
       <h3 className="text-3xl font-bold">Explore Courses</h3>
       <SearchBar
         search={search}
         handleSearch={handleSearch}
         setSearch={setSearch}
       />
-      <div className="grid grid-cols-3 grid-flow-2">
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
-        <CoursesCard creator={1}/>
+      <div className="grid grid-cols-3 grid-flow-2 gap-4">
+        {
+          data.map((value:any, index)=> (
+            <CoursesCard key={index} creator={value.name} description={value.url}/>
+          ))
+        }
       </div>
     </div>
   );
--- components/CoursesCard.tsx
+++ components/CoursesCard.tsx
@@ -1,8 +1,13 @@
+import { Course } from "@/types/Course"
+
 export default function CoursesCard(props:any) {
     return(
-        <div className="flex flex-col max-w-[300px] py-7 px-5 gap-4 bg-white rounded-lg border-2 border-gray-300">
-            <h4 className="text-l font-bold">header</h4>
-            <p>description</p>
+        <div className="flex flex-col max-w-[400px] py-7 px-5 gap-4 bg-white rounded-lg border-2 border-gray-300">
+            <div className="flex justify-between">
+                <h4 className="text-l font-bold">{props.creator}</h4>
+                <div>public</div>
+            </div>
+            <p>description : {props.url}</p>
             <div className="flex justify-between text-gray-500">
                 <div>total topics</div>
                 <div>total quizzes</div>
