3b7d326be5912cf97ebebd3585246fd378335a30
peth02 <oopeth139oo@gmail.com>
2025-12-27 21:41:20 +0800

    copy home to my_learning page

--- app/(Home)/my_learning/page.tsx
+++ app/(Home)/my_learning/page.tsx
@@ -1,42 +1,47 @@
+"use client";
+import CoursesCard from "@/components/CoursesCard";
+import { SearchBar } from "@/components/Searchbar";
+import { useEffect, useState } from "react";
+import { getPokemon } from "@/services/pokemon";
+
 export default function MyLearning() {
-    return(
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
-      <h1 className="text-4xl font-bold">My current courses</h1>
+  const [search, setSearch] = useState<string>("");
+  const [data, setData] = useState([]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getPokemon();
+        console.log("Data loaded:", response.results);
+        setData(response.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen gap-7 py-10 px-20 bg-gray-100">
+      <h3 className="text-3xl font-bold">My Created Courses</h3>
+      <SearchBar
+        search={search}
+        handleSearch={handleSearch}
+        setSearch={setSearch}
+      />
+      <div className="grid grid-cols-3 grid-flow-2 gap-4">
+        {
+          data.map((value:any, index)=> (
+            <CoursesCard key={index} creator={value.name} description={value.url}/>
+          ))
+        }
+      </div>
     </div>
-    );
+  );
 }
