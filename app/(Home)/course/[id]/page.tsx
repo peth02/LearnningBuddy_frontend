2c7c2f5c73fe0e49b5818606336297a5490d3233
peth02 <oopeth139oo@gmail.com>
2026-01-05 16:22:18 +0800

    change contents card components (mv button to outside in  coures/[id])

--- app/(Home)/course/[id]/page.tsx
+++ app/(Home)/course/[id]/page.tsx
@@ -1,33 +1,38 @@
 "use client";
 import { useState } from "react";
 import { Topics, Flashcards, Quizzes } from "@/components/CourseContentTabs";
-import { EditCourseForm } from "@/components/Forms";
+import { CreateQuizForm, EditCourseForm } from "@/components/Forms";
 
 export default function Course() {
   const data = ["1", "2"];
   const [content, setContent] = useState("topic");
-  const [editCourse, setEditCourse ] = useState(false)
+  const [editCourse, setEditCourse] = useState(false);
+  const [createQuiz, setCreateQuiz] = useState(false);
 
   const HandleEdit = () => {
     setEditCourse((prev) => !prev);
-  }
+  };
   return (
     <div className="flex flex-col min-h-screen gap-7 py-10 px-20 bg-gray-100">
       <section className="bg-white rounded-lg shadow-sm p-10">
-        {
-          !editCourse 
-            ? (<div className="flex">
-                <div>
-                  <div>head</div>
-                  <div>description</div>
-                  <div>courses progress</div>
-                </div>
-                <a onClick={HandleEdit} className="ml-auto cursor-pointer h-fit text-gray-600 underline">Edit</a>
-              </div>)
-            : (<EditCourseForm stateChange={HandleEdit} />)
-        }
-      {/* enroll */}
-
+        {!editCourse ? (
+          <div className="flex">
+            <div>
+              <div>head</div>
+              <div>description</div>
+              <div>courses progress</div>
+            </div>
+            <a
+              onClick={HandleEdit}
+              className="ml-auto cursor-pointer h-fit text-gray-600 underline"
+            >
+              Edit
+            </a>
+          </div>
+        ) : (
+          <EditCourseForm stateChange={HandleEdit} />
+        )}
+        {/* enroll */}
       </section>
       <section className="bg-white rounded-lg shadow-sm min-h-screen">
         <nav className="flex border-b-1 border-gray-300">
@@ -36,7 +41,9 @@ export default function Course() {
               setContent("topic");
             }}
             className={`px-10 py-4 cursor-pointer ${
-              content === "topic" ? "text-blue-500 font-bold border-b-2 border-blue" : ""
+              content === "topic"
+                ? "text-blue-500 font-bold border-b-2 border-blue"
+                : ""
             }`}
           >
             Topics
@@ -46,7 +53,9 @@ export default function Course() {
               setContent("flashcard");
             }}
             className={`px-10 py-4 cursor-pointer ${
-              content === "flashcard" ? "text-blue-500 font-bold border-b-2 border-blue" : ""
+              content === "flashcard"
+                ? "text-blue-500 font-bold border-b-2 border-blue"
+                : ""
             }`}
           >
             Flashcards
@@ -56,7 +65,9 @@ export default function Course() {
               setContent("quiz");
             }}
             className={`px-10 py-4 cursor-pointer ${
-              content === "quiz" ? "text-blue-500 font-bold border-b-2 border-blue" : ""
+              content === "quiz"
+                ? "text-blue-500 font-bold border-b-2 border-blue"
+                : ""
             }`}
           >
             Quizzes
@@ -65,12 +76,42 @@ export default function Course() {
         {/* <hr className="mb-3 bg-gray-300 h-[1px] border-none"/> */}
         <div>
           {content == "flashcard" ? (
-            <Flashcards />
+            <div className="p-10">
+              <div className="place-content-end">
+                {/*  only creator can see */}
+                <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
+                  + Create Deck
+                </button>
+              </div>
+              <div className="grid grid-cols-2 gap-5">
+                <Flashcards />
+              </div>
+            </div>
           ) : content == "quiz" ? (
-            <Quizzes />
-          ) : (
-            <Topics />
-          )}
+            <div className="p-10">
+              <div className="place-content-end">
+                {/*  only creator can see */}
+                <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
+                  + Create Quiz
+                </button>
+              </div>
+              <div className="grid grid-cols-2 gap-5">
+                <Quizzes />
+              </div>
+            </div>
+          ) : content == "topic" ? (
+            <div className="p-10">
+              <div className="place-content-end">
+                {/*  only creator can see */}
+                <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
+                  + Add Topic
+                </button>
+              </div>
+              <div className="flex flex-col gap-4">
+                <Topics />
+              </div>
+            </div>
+          ) : null}
           {/* <Topics items={data}/> */}
         </div>
       </section>
--- components/CourseContentTabs.tsx
+++ components/CourseContentTabs.tsx
@@ -1,85 +1,55 @@
 export function Topics(items: any) {
   return (
-    <div className="p-10">
-      <div className="place-content-end">
-        {/*  only creator can see */}
-        <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
-          + Add Topic
-        </button>
+    <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
+      <div className="flex gap-5">
+        <div>1</div>
+        <div className="flex flex-col">
+          <div>topic 1</div>
+          <div>description</div>
+          <div>description</div>
+        </div>
+        <div className="ml-auto">
+          <div>icon1</div>
+          {/*  only creator can see */}
+          <div>icon2</div>
+        </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
-          <div className="flex gap-5">
-            <div>1</div>
-            <div className="flex flex-col">
-              <div>topic 1</div>
-              <div>description</div>
-              <div>description</div>
-            </div>
-            <div className="ml-auto">
-              <div>icon1</div>
-              {/*  only creator can see */}
-              <div>icon2</div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+    </section>
   );
 }
 
 export function Flashcards() {
   return (
-    <div className="p-10">
-      <div className="place-content-end">
-        {/*  only creator can see */}
-        <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
-          + Create Deck
-        </button>
+    <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
+      <div className="flex gap-5">
+        <div>1</div>
+        <div className="flex flex-col">
+          <div>Deck name</div>
+          <div>n topics</div>
+          <div>topic : 1 2 3</div>
+        </div>
+        <div className="ml-auto">
+          <div>edit</div>
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-5">
-        <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
-          <div className="flex gap-5">
-            <div>1</div>
-            <div className="flex flex-col">
-              <div>Deck name</div>
-              <div>n topics</div>
-              <div>topic : 1 2 3</div>
-            </div>
-            <div className="ml-auto">
-              <div>edit</div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+    </section>
   );
 }
 
 export function Quizzes() {
   return (
-    <div className="p-10">
-      <div className="place-content-end">
-        {/*  only creator can see */}
-        <button className="mb-5 mr-auto min-w-[100px] bg-blue-500 text-white font-bold p-2 rounded-lg text-center cursor-pointer hover:bg-blue-600">
-          + Create Quiz
-        </button>
+    <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
+      <div className="flex gap-5">
+        <div>1</div>
+        <div className="flex flex-col">
+          <div>Quiz name</div>
+          <div>n topics</div>
+          <div>topic : 1 2 3</div>
+        </div>
+        <div className="ml-auto">
+          <div>edit</div>
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-5">
-        <section className="bg-white rounded-lg border-2 border-gray-300 px-10 py-7 min-h-[100px]">
-          <div className="flex gap-5">
-            <div>1</div>
-            <div className="flex flex-col">
-              <div>Quiz name</div>
-              <div>n topics</div>
-              <div>topic : 1 2 3</div>
-            </div>
-            <div className="ml-auto">
-              <div>edit</div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+    </section>
   );
 }
