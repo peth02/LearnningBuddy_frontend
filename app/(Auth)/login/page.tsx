d8f5174c199c3a83b2b517a5808011d8f0c2e868
peth02 <oopeth139oo@gmail.com>
2026-01-25 16:57:46 +0700

    add simple form

--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -1,10 +1,47 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Login() {
+
+  const handleLogin = () => {
+
+  }
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Login Page
-        </h1>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50">
+      <main className="flex rounded-lg w-full max-w-3xl flex-col items-center p-16 bg-white items-start">
+        <div>
+          <Link href="/home">home</Link>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
+            Login Page
+          </h1>
+        </div>
+        <form onSubmit={() => console.log("hello")}>
+          <div className="flex flex-col">
+            <label>name</label>
+            <input
+              type="text"
+              aria-label="name"
+              placeholder="Name"
+              maxLength={50}
+              required
+              className=""
+            />
+            <label>email</label>
+            <input
+              type="email"
+              aria-label="email"
+              placeholder="Email"
+              required
+              className=""
+            />
+            <div>
+              <Link href="/register">Already have an account</Link>
+            </div>
+            <button>submit</button>
+          </div>
+        </form>
       </main>
     </div>
   );
